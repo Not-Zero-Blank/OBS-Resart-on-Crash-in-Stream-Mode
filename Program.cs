0d66ed7b480c0bc80a01168f71402e60d84eebf4
YeetBlank1337 <JanTV720@gmail.com>
2021-09-25 18:29:15 +0200

    Version 2

--- Program.cs
+++ Program.cs
@@ -12,43 +12,77 @@ namespace OBS_Restream_on_Crash_by_Blank_Windows_Edition
 {
     class Program
     {
-        static string OBS = SearchOBS();
+        static string OBS = string.Empty;
+        static Process OBSProcess = null;
         static void Main(string[] args)
         {
+            Console.Title = "OBS Restart on Crash";
+            Console.CursorVisible = false;
             Console.WriteLine($"Made by \"Mr. Blank#2709\" if you have issues contact me! ");
             dontclose();
-            if (string.IsNullOrWhiteSpace(OBS))
-            {
-                OBS = FileDialog();
-            }
-            Console.WriteLine($"Found OBS in {OBS}");
-            Environment.CurrentDirectory = Path.GetDirectoryName(OBS);
-            Console.WriteLine("Starting OBS in Normal Mode");
-            OBSNormalMode();
-            Console.WriteLine("Press any Key in the Terminal to Exit");
+            AttachProcesstoOBs();
+            Console.WriteLine("Press Enter in the Terminal to Exit");
         }
-        static void OBSNormalMode()
+        static bool IsObs(string File)
+        {
+            var test = FileVersionInfo.GetVersionInfo(File).FileDescription;
+            if (test == "OBS Studio")
+            {
+                return true;
+            }
+            return false;
+        }
+        static Process GetOBS()
+        {
+            foreach (Process a in Process.GetProcesses())
+            {
+                try
+                {
+                    if (IsObs(a.MainModule.FileName))
+                    {
+                        return a;
+                    }
+                }
+                catch
+                { }
+            }
+            return null;
+        }
+        static void AttachProcesstoOBs()
         {
             try
             {
-                var obs = Process.Start(OBS);
-                obs.EnableRaisingEvents = true;
-                obs.Exited += RestartOBS;
+                OBSProcess = GetOBS();
+                if (OBSProcess == null)
+                {
+                    Console.WriteLine("Await OBS Start\nPlease Start OBS");
+                    while (GetOBS() == null)
+                    {
 
+                    }
+                    OBSProcess = GetOBS();
+                }
+                Console.WriteLine($"Found OBS with Process ID {OBSProcess.Id}");               
+                OBS = OBSProcess.MainModule.FileName;
+                Console.WriteLine($"Found OBS in {OBS}");
+                Environment.CurrentDirectory = Path.GetDirectoryName(OBS);
+                Console.WriteLine("Set Enviroment for OBS");
+                OBSProcess.EnableRaisingEvents = true;
+                OBSProcess.Exited += RestartOBS;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
-                OBSNormalMode();
+                AttachProcesstoOBs();
             }
         }
         static void OBSStreamlMode()
         {
             try
             {
-                var obs = Process.Start(OBS, "--startstreaming");
-                obs.EnableRaisingEvents = true;
-                obs.Exited += RestartOBS;
+                OBSProcess = Process.Start(OBS, "--startstreaming");
+                OBSProcess.EnableRaisingEvents = true;
+                OBSProcess.Exited += RestartOBS;
 
             }
             catch (Exception e)
@@ -59,102 +93,14 @@ namespace OBS_Restream_on_Crash_by_Blank_Windows_Edition
         }
         static void dontclose()
         {
-            new Thread(() => Console.ReadKey()).Start();
+            new Thread(() => Console.ReadLine()).Start();
         }
 
         private static void RestartOBS(object sender, EventArgs e)
         {
-            Console.WriteLine($"OBS Closed Restarting");
+            Console.WriteLine($"OBS Closed ({OBSProcess.ExitTime}) Restarting");
             Console.WriteLine("Starting OBS in Stream Mode");
             OBSStreamlMode();
-        }
-
-        static string SearchOBS()
-        {
-            if (!File.Exists(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit).lnk"))
-            {
-                Console.WriteLine(@"Cant find OBS Studio in C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio    Please Select you own Path");
-                Thread.Sleep(2000);
-                return FileDialog();
-            }
-            return GetShortcutTarget(@"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\OBS Studio\OBS Studio (64bit).lnk");
-        }
-        static string FileDialog()
-        {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())    //skidded from Microsoft Docs :)
-            {
-                openFileDialog.InitialDirectory = @"C:\Program Files";
-                openFileDialog.Filter = "exe files (*.exe)|*.exe|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-                openFileDialog.Title = "SELECT obs64.exe";
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    //Get the path of specified file
-                    return openFileDialog.FileName;
-                }
-                else
-                {
-                    return FileDialog();
-                }
-            }
-        }
-        private static string GetShortcutTarget(string file) // Skiddded from https://blez.wordpress.com/2013/02/18/get-file-shortcuts-target-with-c/
-        {
-            try
-            {
-                if (System.IO.Path.GetExtension(file).ToLower() != ".lnk")
-                {
-                    throw new Exception("Supplied file must be a .LNK file");
-                }
-
-                FileStream fileStream = File.Open(file, FileMode.Open, FileAccess.Read);
-                using (System.IO.BinaryReader fileReader = new BinaryReader(fileStream))
-                {
-                    fileStream.Seek(0x14, SeekOrigin.Begin);     // Seek to flags
-                    uint flags = fileReader.ReadUInt32();        // Read flags
-                    if ((flags & 1) == 1)
-                    {                      // Bit 1 set means we have to
-                                           // skip the shell item ID list
-                        fileStream.Seek(0x4c, SeekOrigin.Begin); // Seek to the end of the header
-                        uint offset = fileReader.ReadUInt16();   // Read the length of the Shell item ID list
-                        fileStream.Seek(offset, SeekOrigin.Current); // Seek past it (to the file locator info)
-                    }
-
-                    long fileInfoStartsAt = fileStream.Position; // Store the offset where the file info
-                                                                 // structure begins
-                    uint totalStructLength = fileReader.ReadUInt32(); // read the length of the whole struct
-                    fileStream.Seek(0xc, SeekOrigin.Current); // seek to offset to base pathname
-                    uint fileOffset = fileReader.ReadUInt32(); // read offset to base pathname
-                                                               // the offset is from the beginning of the file info struct (fileInfoStartsAt)
-                    fileStream.Seek((fileInfoStartsAt + fileOffset), SeekOrigin.Begin); // Seek to beginning of
-                                                                                        // base pathname (target)
-                    long pathLength = (totalStructLength + fileInfoStartsAt) - fileStream.Position - 2; // read
-                                                                                                        // the base pathname. I don't need the 2 terminating nulls.
-                    char[] linkTarget = fileReader.ReadChars((int)pathLength); // should be unicode safe
-                    var link = new string(linkTarget);
-
-                    int begin = link.IndexOf("\0\0");
-                    if (begin > -1)
-                    {
-                        int end = link.IndexOf("\\\\", begin + 2) + 2;
-                        end = link.IndexOf('\0', end) + 1;
-
-                        string firstPart = link.Substring(0, begin);
-                        string secondPart = link.Substring(end);
-
-                        return firstPart + secondPart;
-                    }
-                    else
-                    {
-                        return link;
-                    }
-                }
-            }
-            catch
-            {
-                return "";
-            }
         }
     }
 }
